5f06165941f78a8f94d1d33bb08dae5b82b2c15b
tgramlich <tobias.gramlich@kardex.com>
2026-02-24 12:19:46 +0100

    Action and Move Validator created

--- src/java/domain/Game.java
+++ src/java/domain/Game.java
@@ -3,11 +3,47 @@ package java.domain;
 import java.util.List;
 
 public class Game {
-    private Long id;
-    private List<Player> players;
+    private final Long id;
+    private final List<Player> players;
     private int currentPlayerIndex;
     private TurnPhase phase;
-    private Pile drawPile;
-    private Pile discardPile;
+    private final Pile drawPile;
+    private final Pile discardPile;
     private int round;
+
+    public Game (Long id, List<Player> players, Pile drawPile, Pile discardPile){
+        this.id = id;
+        this.players = players;
+        this.currentPlayerIndex = 0;
+        this.phase = TurnPhase.MUST_DRAW_OR_TAKE_DISCARD;
+        this.drawPile = drawPile;
+        this.discardPile = discardPile;
+        this.round = 1;
+    }
+
+    public Player getCurrentPlayer(){
+        return players.get(currentPlayerIndex);
+    }
+
+    public void changeCurrentPlayer(){
+        if (currentPlayerIndex == players.size() - 1){
+            currentPlayerIndex = 0;
+            round++;
+        }
+        else {
+            currentPlayerIndex++;
+        }
+    }
+
+    public Card drawFromDrawPile(){
+        return drawPile.draw();
+    }
+
+    public Card drawFromDiscardPile(){
+        return discardPile.draw();
+    }
+
+    public void setPhase(TurnPhase phase) {
+        this.phase = phase;
+    }
 }
--- src/java/domain/GameState.java
+++
@@ -1,4 +0,0 @@
-package java.domain;
-
-public enum GameState {
-}
