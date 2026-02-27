97ce466281b3a2733729356257a68ac1cfb1fabc
Melina Klein <mel.klein.2x4@lehre.mosbach.dhbw.de>
2026-02-27 14:19:33 +0100

    class diagramm and mapper interface

---
+++ src/main/java/infrastructure/persistence/mapper/EntityDomainMapper.java
@@ -0,0 +1,8 @@
+package java.infrastructure.persistence.mapper;
+
+import java.infrastructure.persistence.entities.GameEntity;
+
+public interface EntityDomainMapper {
+    public void toGame(Long gameId);
+}
+
